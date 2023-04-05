da3ee92670cf32f78469663092e9eb0db7050dbf
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-04-06 01:18:17 +0600

    data loaded from local storage for orders page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
+  const cart = useLoaderData();
+  console.log(cart);
+
   return (
     <div className="shop-container">
       <div className="products-container">
         <h2>Orders Page</h2>
       </div>
       <div className="cart-container">
-        <Cart cart={[]} />
+        <Cart cart={cart} />
       </div>
     </div>
   );
